4f591502345ebe015bae176733b8c24dc34b2586
Stanislav Naumenko <Naumenko.s.vik@sberbank.ru>
2021-08-24 16:41:04 +0300

    ProblemB solved

--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -2,8 +2,26 @@ class ProblemB {
 
     fun main() {
 
-        //ваша реализация в методе main
-
+        var numberOfStrings = getNumberOfIterations()
+        var listOfStringTemplates = ArrayList<IntArray>()
+        while (numberOfStrings > 0) {
+            listOfStringTemplates.add(readIntArray(2))
+            numberOfStrings--
+        }
+        for (item in listOfStringTemplates) {
+            for (counter in 0 until item[0]) {
+                print((97 + counter % item[1]).toChar())
+            }
+            println()
+        }
     }
 
+    fun getNumberOfIterations(): Int {
+        var numberOfIterations: Int
+        do {
+            // тут бы вывод в консоль что ожидается, но по условиям задачи быть не должно
+            numberOfIterations = readInt()
+        } while (numberOfIterations < 1 || numberOfIterations > 100)
+        return numberOfIterations
+    }
 }
